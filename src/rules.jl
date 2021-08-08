85923bb2a62e408660b6e7782f095e0ea91ba5bb
rikhuijzer <rikhuijzer@pm.me>
2021-08-08 17:15:36 +0200

    Almost there

--- src/rules.jl
+++ src/rules.jl
@@ -4,22 +4,33 @@
 Return whether rule 2 is applicable given graph `G` with Y, Z, X and W.
 In other words, return true if ``(Y \\perp Z | X, W)_{G_{\\overline{x} \\underline{z}}}``.
 """
-function rule2_applicable(G::SimpleDiGraph, Y, Z, X, W)::Bool
+function rule2_applicable(G::AbstractGraph, Y, Z, X, W)::Bool
     G = without_incoming(G, Set(X))
     G = without_outgoing(G, Set(Z))
     isempty(nodes(G)) && return false  # Or true? Not sure.
     return d_separated(G, Y, Z, X)
 end
 
-function rule2(G::MetaDiGraph, y, z, x, w)
-    return true
+sym2uppercase(s::Symbol) = Symbol(uppercase(string(s)))
+sym2uppercase(s::SymbolicUtils.Sym{Number, Nothing}) = sym2uppercase(s.name)
+
+function rule2(G::MetaDiGraph, y, z, x; w=nothing)
+    Y = name2node(G, sym2uppercase(y))
+    @show Y
+    Z = name2node(G, sym2uppercase(z))
+    Z = Set(Z)
+    X = name2node(G, sym2uppercase(x))
+    W = isnothing(w) ? Set() : name2node(G, sym2uppercase(w))
+    out = rule2_applicable(G, Y, Z, X, W)
+    @show out
+    return out
 end
 
 function rule2(G::MetaDiGraph)
     # Make sure to put a simplified version at the lhs
     # (https://github.com/JuliaSymbolics/SymbolicUtils.jl/issues/331).
 
-    r = @acrule P(~y, ~z + Do(~x) + Do(~w)) => rule2(G, ~y, ~z, ~x, ~w) ? P(~y, ~z + Do(~x) + ~w) : nothing
+    r = @acrule P(~y, ~z + Do(~x)) => rule2(G, ~y, ~z, ~x) ? P(~y, ~z + ~x) : nothing
     return r
 end
 
--- test/rules.jl
+++ test/rules.jl
@@ -1,7 +1,16 @@
 @testset "rule2" begin
-    @syms a b c d e
+    @syms u x z y
 
-    G = MetaDiGraph()
+    # From the Causality book (2009) around page 86.
+    edges = [
+        :U => :X,
+        :U => :Y,
+        :X => :Z,
+        :Z => :Y
+    ]
+    G = graph(edges)
     r = rule2(G)
-    @eqtest r(P(a, b + Do(c) + Do(d))) == P(a, b + Do(c) + d)
+    @eqtest r(P(y, x + Do(z))) == P(y, x + z)
+    # @eqtest r(P(u, b + Do(c) + Do(d))) == P(a, b + Do(c) + d)
+
 end
